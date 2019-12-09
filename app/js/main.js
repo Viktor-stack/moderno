11675440d86711d7910ff0c494e4fd265043e887
Viktor-stack <grohik493@gmail.com>
2019-12-09 05:25:14 +0200

    start project

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $('.product-slider__inner').slick({
+    arrows: false,
+    dots:true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+  $('.reiteng-user').rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  var mixer = mixitup('.propducts__inner-box');
+
+
+
+});
