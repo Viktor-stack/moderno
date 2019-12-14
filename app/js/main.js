d0df2eabc50c728289d18192bb42adb6dc47e417
Viktor-stack <grohik493@gmail.com>
2019-12-14 07:14:12 +0200

    added aside and poduct-page content and js

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,29 @@
 $(function(){
 
+  $(".range__price-limit").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$",  
+  });
 
   $('.reiteng-user').rateYo({
     rating: 5,
     starWidth: "12px",
     readOnly: true,
+  });
+
+  $('.icon-th-list').on('click',function(){
+    $('.addclass').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active'); 
+  });
+  $('.icon-th-large').on('click',function(){
+    $('.addclass').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
   });
 
   var mixer = mixitup('.propducts__inner-box');
