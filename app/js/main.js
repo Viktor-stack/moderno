6225ff73e4f9fc5a35f2ce37485f140fa903eac7
Viktor-stack <grohik493@gmail.com>
2019-12-21 14:39:53 +0200

    added personal-info page

--- app/js/main.js
+++ app/js/main.js
@@ -43,6 +43,15 @@ $(function(){
     return false;
   });
 
+  $('.settings__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.settings__inner').find('.tab-item').removeClass('active-tab').hide();
+    $('.settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
 
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
