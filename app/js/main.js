6da43b3755209c608a4bd15c2412c62aec6ae145
Viktor-stack <grohik493@gmail.com>
2019-12-23 04:31:07 +0200

    added profile details

--- app/js/main.js
+++ app/js/main.js
@@ -51,7 +51,18 @@ $(function(){
     $('#' + id).addClass('active-tab').fadeIn();
     return false;
   });
-
+  
+  $('.profile-details__inner .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $(".about__me-inner")
+      .find(".tab-item")
+      .removeClass("active-tab")
+      .hide();
+    $('.profile-details__inner .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
 
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
